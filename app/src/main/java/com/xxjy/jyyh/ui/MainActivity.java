b98d970df418e890ed7bfb9c8774ed87642d4df1
qinyanhui <power_android@163.com>
2021-02-25 18:27:01 +0800

    6.0.2,修复tab展示问题

--- app/src/main/java/com/xxjy/jyyh/ui/MainActivity.java
+++ app/src/main/java/com/xxjy/jyyh/ui/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends BindingActivity<ActivityMainBinding, MainViewM
                 UserConstants.setGoneIntegral(true);
                 mBinding.navView.getMenu().removeItem(R.id.navigation_home);
                 mBinding.navView.getMenu().removeItem(R.id.navigation_integral);
-            }else{
+            }else {
                 UserConstants.setGoneIntegral(false);
             }
         });
